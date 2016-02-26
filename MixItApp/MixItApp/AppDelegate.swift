b91e38b0ed3f60976f0478560f8554f1e759aa89
limic900 <micli@ucdavis.edu>
2016-02-25 19:32:32 -0800

    Splash Screen Duration
    Increased from 3 seconds to 10 seconds.

--- MixItApp/MixItApp/AppDelegate.swift
+++ MixItApp/MixItApp/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        NSThread.sleepForTimeInterval(3)
+        NSThread.sleepForTimeInterval(10)
         return true
     }
 
