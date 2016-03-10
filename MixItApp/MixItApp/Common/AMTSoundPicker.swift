8c6d4ae3a41d4d641588c98a26cc20c02ba4f0b3
baixiangguobing <qhfan@gmail.com>
2016-03-10 09:47:41 -0800

    update

---
+++ MixItApp/MixItApp/Common/AMTSoundPicker.swift
@@ -0,0 +1,81 @@
+//
+//  AMTSoundPicker.h
+
+//  Track Picker class for seleting invdividual tracks from gallery
+import Foundation
+import MediaPlayer
+
+
+let utility = Common.utility()
+let amtTrackManager = Common.amtTrackManager()
+
+// The delegate protocol to control sound picker events(selected, cancelled)
+protocol AMTSoundPickerDelegate {
+    func addAudio(track: AMTTrack)
+}
+class AMTSoundPicker: NSObject, MPMediaPickerControllerDelegate {
+    var parentVC: UIViewController?
+    var delegate : AMTSoundPickerDelegate?
+    /**
+     * Get singletone instance of this class
+     */
+    private static let soundPicker = AMTSoundPicker()
+    class func sharedInstance() -> AMTSoundPicker {
+        return soundPicker
+    }
+    /**
+     * Present audio picker to select tracks to be used in multitrack file generation.
+     *
+     * @param: delegate The delegate to control sound picker events(selected, cancelled)
+     * imported file.
+     */
+    
+    func presentAudioPicker(parentVC: UIViewController, delegate: AMTSoundPickerDelegate) {
+        self.parentVC = parentVC
+        self.delegate = delegate
+        let soundPicker: MPMediaPickerController = MPMediaPickerController(mediaTypes: .AnyAudio)
+        soundPicker.delegate = self
+        soundPicker.allowsPickingMultipleItems = false
+        parentVC.presentViewController(soundPicker, animated: true, completion: { _ in })
+    }
+    
+    func mediaPicker(mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        let item = mediaItemCollection.items[0]
+        let assetURL = item.assetURL
+        let assetTitle = item.title
+        let assetArtist = item.artist
+        let title: String = (assetTitle == nil) ? "Unknown Title" : assetTitle!.uppercaseString
+        let artist: String = (assetArtist == nil) ? "Unknown Artist" : assetArtist!.uppercaseString
+        let trackName: String = "\(title) by \(artist)"
+        if assetURL != nil {
+            DSBezelActivityView.newActivityViewForView(parentVC!.view!)
+            if amtTrackManager.currentProjectName.characters.count == 0 {
+                amtTrackManager.currentProjectName = ProjectManager.newProject().lastPathComponent
+            }
+            let outputURL: NSURL = Common.getClipAudioPath(assetURL, project: amtTrackManager.currentProjectName, trackName: trackName)!
+            utility.exportAudioAtURL(assetURL!, outputURL: outputURL, completionBlock: {(strOutpath: AnyObject) -> Void in
+                DSBezelActivityView.removeViewAnimated(true)
+                let track: AMTTrack = AMTTrack()
+                track.trackName = trackName
+                track.audioURL = strOutpath as? NSURL
+                track.trackVolume = 1.0
+                amtTrackManager.arrTracks?.append(track)
+                if self.delegate != nil {
+                    self.delegate?.addAudio(track)
+                }
+                }, WithFailure: {(errorStr: String) -> Void in
+                    DSBezelActivityView.removeViewAnimated(true)
+            })
+            parentVC!.dismissViewControllerAnimated(true, completion: { _ in })
+        }
+        else {
+            parentVC!.dismissViewControllerAnimated(true, completion: { _ in })
+            IVMAlertController.showNoAudioExistAlert(parentVC!)
+        }
+    }
+    
+    func mediaPickerDidCancel(mediaPicker: MPMediaPickerController) {
+        parentVC!.dismissViewControllerAnimated(true, completion: { _ in })
+    }
+}
+
