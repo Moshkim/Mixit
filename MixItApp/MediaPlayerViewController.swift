c00d84d83241fe80e65d47636cd5a4297ca59509
Moses <rlakim@ucdavis.edu>
2016-05-24 08:59:36 -0700

    just checking

--- MixItApp/MediaPlayerViewController.swift
+++ MixItApp/MediaPlayerViewController.swift
@@ -56,7 +56,7 @@ class MediaPlayerViewController: UIViewController,UIImagePickerControllerDelegat
         super.viewDidLoad()
         images = NSMutableArray(array:["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"])
         
-        carouselView.type = .TimeMachine
+        carouselView.type = .CoverFlow2
         carouselView.reloadData()
         
         RatingView.didTouchCosmos = didTouchCosmos
